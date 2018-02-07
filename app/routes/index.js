25fc20d5e7133465e1f4120770a544ec1d3448e1
Navaneedhan <navaneedhan.cit@gmail.com>
2018-02-07 10:15:55 +0530

    Home controller addition: Feb 07

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var home_controller = require('../controllers/homeController');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', home_controller.index);
 
 module.exports = router;
