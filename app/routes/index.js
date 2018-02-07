88a1550eed5f19a3109dfddee3b0c5305dcfaeda
Nivethitha <nivethithamurugan@gmail.com>
2018-02-07 21:57:16 +0530

    Adding

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,5 +5,14 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/Home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+router.get('/Salary', function(req, res, next) {
+  res.render('index', { title: 'Salary' });
+});
+router.get('/Expense', function(req, res, next) {
+  res.render('index', { title: 'Expense' });
+});
 
 module.exports = router;
